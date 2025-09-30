70b19bc899022f1c5ee7f531da3e7705df427343
sangminna <sm9171@nate.com>
2025-09-30 20:16:29 +0900

    Initial commit

---
+++ src/main/java/com/kbhealthcare/KbHealthcareApplication.java
@@ -0,0 +1,13 @@
+package com.kbhealthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbHealthcareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KbHealthcareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kbhealthcare/KbHealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbhealthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbHealthcareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
